865c4e69ccfcce25f28064ce29a675aaaa2ef55c
Chad Malla <cmalla@sfu.ca>
2017-11-07 16:11:00 -0800

    adding stack nav and homescreen

---
+++ appyhour/src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Component} from 'react-native'
+
+
+
+
+
+export default class HomeScreen extends Component {
+  render(){
+    return(
+      <View>
+        <Text> HomeScreen </Text>
+      </View>
+    )
+  }
+
+}
